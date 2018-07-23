b2b5d627575caad18ca655e47da78d75351ff8da
Brandux <branduxjuarez@upeu.edu.pe>
2018-07-23 00:10:53 -0500

    avance

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'niamul@themerox.com'; //put your email here
+    $to         = 'tecnologias.vernie@gmail.com'; //put your email here
 
     $email_template = 'simple.html';
 
